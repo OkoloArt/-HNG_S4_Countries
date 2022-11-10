5ee009ff6eefad7f35f9683c4a608ec5e06aba74
Arthur Okolo <Okoloarthur4@gmail.com>
2022-11-10 20:40:44 +0100

    Allowing internet uses permission, creating module file for Dependency Injection

--- app/src/main/java/com/example/countries/di/AppModule.kt
+++ app/src/main/java/com/example/countries/di/AppModule.kt
@@ -1,5 +1,34 @@
 package com.example.countries.di
 
-class AppModule
+import com.example.countries.common.Constants
+import com.example.countries.data.remote.CountriesApi
+import com.example.countries.data.repository.CountriesRepositoryImpl
+import com.example.countries.domain.repository.CountriesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
 {
+    @Provides
+    @Singleton
+    fun provideCountriesApi(): CountriesApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CountriesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountriesRepository(api: CountriesApi):CountriesRepository{
+        return CountriesRepositoryImpl(api)
+    }
 }
