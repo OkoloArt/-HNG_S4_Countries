ae01fe52fddef17f180750b091ae137e1fd5a47e
Arthur Okolo <Okoloarthur4@gmail.com>
2022-11-11 15:50:53 +0100

    observing network changes

--- app/src/main/java/com/example/countries/di/AppModule.kt
+++ app/src/main/java/com/example/countries/di/AppModule.kt
@@ -1,12 +1,16 @@
 package com.example.countries.di
 
+import android.content.Context
 import com.example.countries.common.Constants
 import com.example.countries.data.remote.CountriesApi
 import com.example.countries.data.repository.CountriesRepositoryImpl
 import com.example.countries.domain.repository.CountriesRepository
+import com.example.countries.utils.ConnectivityObserver
+import com.example.countries.utils.NetworkConnectivityObserver
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -31,4 +35,11 @@ object AppModule
     fun provideCountriesRepository(api: CountriesApi):CountriesRepository{
         return CountriesRepositoryImpl(api)
     }
+
+    @Provides
+    @Singleton
+     fun provideConnectivityObserver(@ApplicationContext context: Context): ConnectivityObserver
+    {
+         return NetworkConnectivityObserver(context)
+     }
 }
