622a28bab707c0b6436c2c96719616fe23be2cfc
Arthur Okolo <Okoloarthur4@gmail.com>
2022-11-11 16:01:36 +0100

    deleted files

--- app/src/main/java/com/example/countries/data/remote/dto/country/Eng.kt
+++
@@ -1,6 +0,0 @@
-package com.example.countries.data.remote.dto.country
-
-data class Eng(
-    val common: String,
-    val official: String
-)
--- app/src/main/java/com/example/countries/data/remote/dto/country/Maps.kt
+++
@@ -1,6 +0,0 @@
-package com.example.countries.data.remote.dto.country
-
-data class Maps(
-    val googleMaps: String,
-    val openStreetMaps: String
-)
